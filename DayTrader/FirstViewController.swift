2ef25b0bcdbcc3ab30e6000129a13ff800b232e7
Matthew Mohrman <matthew.mohrman@slalom.com>
2017-07-07 11:37:34 -0500

    Initial Commit

---
+++ DayTrader/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DayTrader
+//
+//  Created by Matthew Mohrman on 7/7/17.
+//  Copyright © 2017 Matthew Mohrman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
